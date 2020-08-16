f2168b0337a5d2a911e03bc1f22b56e5a06d077d
Raphael Kim <rageworx@gmail.com>
2020-08-16 14:57:58 +0900

    removing some files to be generated instantly.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 19 */
-/* SMP */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#19 SMP Wed Aug 12 15:11:59 KST 2020"
-#define LINUX_COMPILE_BY "raphkay"
-#define LINUX_COMPILE_HOST "raphkay-meditdev"
-#define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) "
